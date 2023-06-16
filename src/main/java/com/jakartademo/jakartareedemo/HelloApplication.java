1f6011d0257905f79eb8d8366346818c9d123ce7
Kb_hacks <kamar254baraka@gmail.com>
2023-06-16 15:21:05 +0300

    Initial commit

---
+++ src/main/java/com/jakartademo/jakartareedemo/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.jakartademo.jakartareedemo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
